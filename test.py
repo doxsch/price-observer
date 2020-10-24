dbca4d18bab89746549054ac9e63a10dd2ab368f
doxsch <28098153+doxsch@users.noreply.github.com>
2020-10-24 15:03:18 +0200

    init

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print("This line will be printed.")
+import requests
+from urllib.request import urlopen
+
+
+def main():
+    print("python main function")
+    # get html from site and write to local file
+    url = 'https://google.com'
+    headers = {'Content-Type': 'text/html', }
+    response = requests.get(url, headers=headers)
+    html = response.text
+    with open('google_com_html', 'w') as f:
+        f.write(html)
+
+
+if __name__ == '__main__':
+    main()
